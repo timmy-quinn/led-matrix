93a74b58f8aed93cbaa098752ba04a4d70f0b02b
timmy-quinn <tquinn5301@gmail.com>
2025-05-15 23:58:06 -0400

    Made cooler plasma animation

--- plasma_state.cpp
+++ plasma_state.cpp
@@ -2,14 +2,14 @@
 // More info can be found https://en.wikipedia.org/wiki/Plasma_effect
 #include "plasma_state.h"
 #include "led_matrix.h"
-#define FRAME_SCALE 0.15
+#define FRAME_SCALE 0.3
 #define INCREMENT_MAX 0xffff
 
 static uint16_t float_hue_to_u16(float hue) {
   return (hue * (0xffff/2)) + 0xffff/2; 
 }
 
-static uint32_t get_pixel_color(Adafruit_NeoPixel *pixels, float x_base, float y_base) {
+static uint16_t get_pixel_color(Adafruit_NeoPixel *pixels, float x_base, float y_base) {
   float hue; 
   float x = x_base * (2 * 3.1415) / PIXEL_COLUMNS * FRAME_SCALE; 
   float y = y_base * (2 * 3.1415) / PIXEL_ROWS * FRAME_SCALE; 
@@ -20,39 +20,43 @@ static uint32_t get_pixel_color(Adafruit_NeoPixel *pixels, float x_base, float y
   hue += sin(x * x - (y * y * 2)); 
   hue+=sin(y-x); 
   hue /=3;  
-  uint16_t hue_normalized = float_hue_to_u16(hue); 
+  return float_hue_to_u16(hue); 
   
-  return pixels->ColorHSV(hue_normalized);
+
 }
 
 static void calc_frame(int increment, Adafruit_NeoPixel *pixels) {
-  float hue;
   float wibble = sin(increment / INCREMENT_MAX);  
   float x_norm, y_norm;
-  uint32_t color;
+  uint16_t hue;
   float step = 0.02;
-  for(float i = -1; i < 1; i+=step) {
-    for (int x = 0; x < PIXEL_COLUMNS; x++) {
-        for (int y = 0; y < PIXEL_ROWS; y++) {
-            color = get_pixel_color(pixels, x * i, y * (-1 *i)); 
-            // color = get_pixel_color(pixels, x, y); 
-            setArrColor(pixels, y, x, color);
-        }
+  uint32_t color;
+  for(uint16_t hue_offset = 0; hue_offset < 0xffff; hue_offset+=2048) {
+    for(float i = -1; i < 1; i+=step) {
+      for (int x = 0; x < PIXEL_COLUMNS; x++) {
+          for (int y = 0; y < PIXEL_ROWS; y++) {
+              hue = get_pixel_color(pixels, x * i, y * (-1 *i)); 
+              // color = get_pixel_color(pixels, x, y); 
+              color = pixels->ColorHSV(hue + hue_offset); 
+              setArrColor(pixels, y, x, color);
+          }
+      }
+      pixels->show();
+      delay(20);   
     }
-    pixels->show();
-    delay(20);   
-  }
 
-for(float i = 1; i > -1; i-=step) {
-    for (int x = 0; x < PIXEL_COLUMNS; x++) {
-        for (int y = 0; y < PIXEL_ROWS; y++) {
-            color = get_pixel_color(pixels, x * i, y * (-1 *i)); 
-            // color = get_pixel_color(pixels, x, y); 
-            setArrColor(pixels, y, x, color);
-        }
+    for(float i = 1; i > -1; i-=step) {
+      for (int x = 0; x < PIXEL_COLUMNS; x++) {
+          for (int y = 0; y < PIXEL_ROWS; y++) {
+              hue = get_pixel_color(pixels, x * i, y * (-1 *i)); 
+              // color = get_pixel_color(pixels, x, y); 
+              color = pixels->ColorHSV(hue + hue_offset); 
+              setArrColor(pixels, y, x, color);
+          }
+      }
+      pixels->show();
+      delay(20);   
     }
-    pixels->show();
-    delay(20);   
   }
   // delay(100); 
 }
