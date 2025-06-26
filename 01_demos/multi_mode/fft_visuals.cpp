9761470b366faed9dbfa63b4e94b2e624647c16a
timmy-quinn <tquinn5301@gmail.com>
2025-06-26 19:56:44 -0400

    Updated multi mode demo

--- 01_demos/multi_mode/fft_visuals.cpp
+++ 01_demos/multi_mode/fft_visuals.cpp
@@ -15,9 +15,13 @@ const size_t num_columns = 8;
 const float min_frequency = 800; 
 
 const size_t min_display_rows = 1; 
-const size_t max_display_rows = 8;  
-const size_t magnitude_division =200; 
-const size_t magnitude_offset = 1350; 
+const size_t max_display_rows = 8; 
+const size_t magnitude_division =75; 
+const size_t magnitude_offset = 1300; 
+const uint16_t average_weight = 3; 
+const uint32_t normalizing_offsets[num_columns] = {18000, 225, 400, 650, 900, 1100, 1300, 1400}; 
+const uint32_t normalizing_scales[num_columns] = {75, 75, 75, 37, 37, 75, 75, 75}; 
+static uint16_t binsPerColumn[num_columns] = {3, 4, 6, 12, 25, 47, 92, 323}; 
 
 AudioInfo info(samples_per_second, channel_count, bits_per_sample);
 I2SStream i2sStream; // Access I2S as stream
@@ -67,10 +71,14 @@ static size_t runningAverageHeights[num_columns] = {0};
 static int runningAverageMagnitude[num_columns] = {0}; 
 
 static void runningAverage() {
-  size_t height = min_display_rows;
-  int current_magnitude;  
   for(size_t col = 0; col < num_columns; col++) {
     runningAverageMagnitude[col] = (static_cast <int>(channels[col]) + runningAverageMagnitude[col])/ 2; 
+  }
+}
+
+static void normalize() {
+  for(size_t col = 0; col < num_columns; col++) {
+    channels[col] = channels[col]; 
   }
 }
 
@@ -78,42 +86,61 @@ static void runningAverage() {
 static void averageBinsToColumns() {
     float diff;
     auto magnitudes = fft.magnitudes(); 
+    size_t fft_size = fft.size(); 
     float start_freq, end_freq, avg_mag;
     size_t bins_per_column = fft_size/num_columns;
-    size_t bin; 
+    size_t bin = 0; 
 
     for(size_t i = 0; i < num_columns; i++) {
-      avg_mag = 0; 
+      channels[i] = 0;  
+      // Human perception of sound is not linear. We will group the lower frequency bins
+      // in smaller groups. 
+      // This will provide a more visually pleasing affect, and be a little more 
+      // reflective of how the sound is perceived
       for(size_t j = 0; j < bins_per_column; j++) {
-        bin = i * bins_per_column + j; 
-        if(fft.frequency(bin) < min_frequency) {
-          continue; 
-        }
-        avg_mag += magnitudes[bin] / bins_per_column; 
+        channels[i] += magnitudes[bin] / bins_per_column; 
+        bin++;  
       }
-      channels[i] = avg_mag; 
-    } 
+    }
 }
 
-static size_t mapMagnitudeToHeight(int magnitude) {
-  size_t corrected_magnitude = (magnitude > magnitude_offset) ? magnitude - magnitude_offset : 0; 
-  return static_cast <size_t>( corrected_magnitude / magnitude_division);
+static size_t mapMagnitudeToHeight(int magnitude, uint32_t offset, uint32_t scale) {
+  size_t corrected_magnitude = (magnitude > offset) ? magnitude - offset : 0; 
+  Serial.printf("%u, ", corrected_magnitude); 
+  return min(static_cast <size_t>( corrected_magnitude / scale), static_cast<size_t>(PIXEL_COLUMNS -1));
+}
+
+static void displayFullColumns(uint16_t col) {
+  uint16_t height = mapMagnitudeToHeight(runningAverageMagnitude[col], normalizing_offsets[col], normalizing_scales[col]); 
+  for(size_t row = 0; row < PIXEL_ROWS; row++) {
+    if(row <= height) {
+      setArrColor(pixels, row, col, channel_colors[col]); 
+    }
+    else {
+      setArrColor(pixels, row, col, 0);         
+    }
+  }
+}
+
+static void displayColumnTops(uint16_t col) {
+  uint16_t height = mapMagnitudeToHeight(runningAverageMagnitude[col], normalizing_offsets[col], normalizing_scales[col]); 
+  for(size_t row = 0; row < PIXEL_ROWS; row++) {
+    if(row == height) {
+      setArrColor(pixels, row, col, channel_colors[col]); 
+    }
+    else {
+      setArrColor(pixels, row, col, 0);         
+    }
+  }
 }
 
 static void displayColumns() {
-  size_t height; 
+
   for(size_t col = 0; col < num_columns; col++) {
-    height = mapMagnitudeToHeight(runningAverageMagnitude[col]); 
-    for(size_t row = 0; row < PIXEL_ROWS; row++) {
-      if(row <= height) {
-        setArrColor(pixels, row, col, channel_colors[col]); 
-      }
-      else {
-        setArrColor(pixels, row, col, 0);         
-      }
-    }
+    displayColumnTops(col);
   }
   pixels->show();
+  Serial.println(); 
 }
 
 void displayFFT(AudioFFTBase &fft){
@@ -130,16 +157,19 @@ void fft_visuals_state::entry() {
     initColumnColors(); 
     
     auto cfgRx = analogIn.defaultConfig(RX_MODE);
+
     cfgRx.copyFrom(info);
     cfgRx.adc_channels[0] = ADC_CHANNEL_4;
     analogIn.begin(cfgRx);
     
     auto fft_cfg = fft.defaultConfig(); 
-    fft_cfg.length = 512; 
+    fft_cfg.window_function = new BufferedWindow(new Hamming());
+    fft_cfg.length = 2048; 
     fft_cfg.channels = channel_count;
     fft_cfg.sample_rate = samples_per_second; 
     fft_cfg.bits_per_sample = bits_per_sample; 
     fft_cfg.callback = &displayFFT; 
+
     fft.begin(fft_cfg);
 
     Serial.printf("buffer size: %u\n", copier.bufferSize()); 
